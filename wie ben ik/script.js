4c89c929d854bb50092240ea847e27bf484974d5
Achraf <pro.achraf.allali@gmail.com>
2022-02-01 16:44:06 +0100

    update

---
+++ wie ben ik/script.js
@@ -0,0 +1,12 @@
+'use strict';
+console.log ('Achraf oef werkt')
+let h1Element = document.querySelector('.h1');
+console.log (h1Element);
+h1Element.style.backgroundColor = 'blue'
+//
+let achrafElement = document.querySelector('#Achraf');
+achrafElement.style.textDecoration = "underline";
+
+
+
+
