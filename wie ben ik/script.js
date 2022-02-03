bcfacade1548ccbf34714cc4f07060f14baff80e
Achraf <pro.achraf.allali@gmail.com>
2022-02-03 16:35:54 +0100

    final

--- wie ben ik/script.js
+++ wie ben ik/script.js
@@ -7,6 +7,20 @@ h1Element.style.backgroundColor = 'blue'
 let achrafElement = document.querySelector('#Achraf');
 achrafElement.style.textDecoration = "underline";
 
+//
+
+let klasseNaam = 'vijfde-rij';
+
+let studenten = document.getElementsByClassName(klasseNaam);
+console.log (studenten);
+//
+for (let element of studenten){
+    console.log(studenten)
+    element.style.backgroundColor = 'purple'
+}
+    
+
+
 
 
 
