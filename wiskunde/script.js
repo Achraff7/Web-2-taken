337eb4bd9563fa4ec5e1eaa9a31f199e39152f08
Achraf <pro.achraf.allali@gmail.com>
2022-02-08 14:10:51 +0100

    update

---
+++ wiskunde/script.js
@@ -0,0 +1,7 @@
+// alert gaat een pop-up tonen met een bericht
+// hier is het bericht de som van het geal 12 + 13
+let getal1 = prompt('geef het eerste getal');
+let getal2 = prompt('geef de tweede getal');
+console.log
+let som = (getal1 * 1) + (getal2 * 1);
+alert(som);
