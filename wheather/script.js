a678c6e0a23d6cd3e4c45693c91bd678284b484c
Achraf <pro.achraf.allali@gmail.com>
2022-02-17 12:06:12 +0100

    final

--- wheather/script.js
+++ wheather/script.js
@@ -1,20 +1,38 @@
 const select = document.querySelector('select');
-  // const html = document.querySelector('.output');
-  const html = document.querySelector('body');
+// const html = document.querySelector('.output');
+const html = document.querySelector('body');
 
-  function updateBgTextColor(bgColor, textColor) {
+function updateBgTextColor(bgColor, textColor) {
     html.style.backgroundColor = bgColor;
     html.style.color = textColor;
-  }
+}
 
-  select.onchange = function () {
+select.onchange = function () {
     const choice = select.value;
+    console.log('optie', choice);
 
     // ADD SWITCH STATEMENT
     // HIERBOVEN NIETS VERANDEREN
     // je code met switch case komt vanaf hier:
-    
+
 
     // tot hier
     // HIERONDER NIETS VERANDEREN
-  }
+    switch (choice) {
+        case "sunny": {
+            updateBgTextColor('yellow', 'black');
+        }
+            break;
+        case "rainy": {
+            updateBgTextColor('blue', 'white');
+        }
+            break;
+        case "snowing": {
+            updateBgTextColor('white', 'black')
+        }
+            break;
+        case "overcast": {
+            updateBgTextColor('grey', 'white')
+        }
+    }
+}
