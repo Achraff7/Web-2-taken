ef869782657df539e16057ab45d1ef21c3ffcaa0
Achraf <pro.achraf.allali@gmail.com>
2022-03-08 16:23:04 +0100

    update

--- functie/script.js
+++ functie/script.js
@@ -1,16 +1,19 @@
 let voetballer1 = { naam: "Lukaku", leeftijd: 28, team: "Chelsea"};
 let voetballer2 = { naam: "Ronaldo", leeftijd: 35, team: "ManUntd"};
 let voetballer3 = { naam: "Messi", leeftijd: 34, team: "PSG"};
-let lijstobjescten = [voetballer1, voetballer2, voetballer3]
+let lijstVoetballer = [voetballer1, voetballer2, voetballer3]
 
-for (let i = 0; i < 5; i++) {
-    console.log(i);
+
+
+console.log(lijstVoetballer.length);
+
+
+for (let i = 0; i < lijstVoetballer.length; i++) {
+    voetballer(lijstVoetballer[i]);
+
 }
 
-console.log(lijstobjescten.length);
-
-
-for (let i = 0; i < lijstobjescten.length; i++) {
-    console.log(lijstobjescten[i]);
+function voetballer(parameter) 
+{ console.log(parameter.naam + ' '+ parameter.leeftijd + ' '+ parameter.team);
 
 }
